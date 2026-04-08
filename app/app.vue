a55ecccc0b4eb130c1bfa2e1f0f0298bec3e541d
Jian Z'car Esteban <91779567+JianZcar@users.noreply.github.com>
2026-04-08 09:44:21 +0800

    config

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="navbar bg-base-100 shadow-sm">
+    <a class="text-xl">PHASE</a>
   </div>
+  <NuxtPage />
 </template>
