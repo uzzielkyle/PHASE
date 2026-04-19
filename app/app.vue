428656b7301b3848a966537d44955407e0bf7345
Uzziel Kyle Ynciong <uzzielynciong@gmail.com>
2026-04-19 20:29:59 +0800

    (config): add eslint and prettier

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="navbar bg-base-100 shadow-sm">
-    <a class="text-xl">PHASE</a>
-  </div>
-  <NuxtPage />
+	<div class="navbar bg-base-100 shadow-sm">
+		<a class="text-xl">PHASE</a>
+	</div>
+	<NuxtPage />
 </template>
